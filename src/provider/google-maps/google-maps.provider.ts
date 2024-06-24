d0ede2bfffeae0135dcac525e6b75317f6738800
Vladislav Dobrea <vladislav@squareup.com>
2024-06-24 20:27:46 +0300

    refactor(provider): adds signing to google maps provider requests geocode and reverse
    ref #FSMWO-1806

--- src/provider/google-maps/google-maps.provider.ts
+++ src/provider/google-maps/google-maps.provider.ts
@@ -9,10 +9,10 @@ import {
 } from './command';
 
 export class GoogleMapsProvider extends AbstractHttpProvider {
-    constructor(httpClient: AxiosInstance, apiKey: string) {
+    constructor(httpClient: AxiosInstance, apiKey: string, secret?: string) {
         super({
-            geocode: new GoogleMapsGeocodeCommand(httpClient, apiKey),
-            reverse: new GoogleMapsReverseCommand(httpClient, apiKey),
+            geocode: new GoogleMapsGeocodeCommand(httpClient, apiKey, secret),
+            reverse: new GoogleMapsReverseCommand(httpClient, apiKey, secret),
             suggest: new GoogleMapsSuggestCommand(httpClient, apiKey),
             placeDetails: new GoogleMapsPlaceDetailsCommand(httpClient, apiKey),
             distance: new GoogleMapsDistanceCommand(httpClient, apiKey),
